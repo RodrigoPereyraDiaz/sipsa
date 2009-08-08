aa9a18745b697b26037f7db7f769fa2d0096c8bc
elsupergomez <elsupergomez@9ee85266-8456-11de-8b65-cf205d531c0e>
2009-08-08 20:26:23 +0000

    Importacion Inicial del Proyecto

---
+++ trunk/Sipsa/src/sipsa/controles/ProductosControl.java
@@ -0,0 +1,36 @@
+/*
+ * Sistemas de Informacion II 2009
+ * Proyecto Sipsa
+ */
+
+package sipsa.controles;
+
+import java.io.IOException;
+import java.text.ParseException;
+import javax.swing.JFileChooser;
+import sipsa.persistencia.FachadaPersistencia;
+
+
+/**
+ * Controlador de Productos
+ * @author Claudio Rodrigo Pereyra Diaz
+ * @author Maria Eugenia Sanchez
+ */
+public class ProductosControl {
+
+    private FachadaPersistencia persistencia = new FachadaPersistencia();
+    /**Muestra un dialogo de seleccion de archivo para realizar la importacion de Productos
+     * Realiza la importar de un archivo y devuelve una lista de productos
+     * @throws IOException 
+     * @throws ParseException
+     */
+    public void ImportarProductosDesdeArchivo() throws IOException, ParseException{
+        //@TODO revisar las exepciones
+        JFileChooser jfileChooser = new JFileChooser();
+        int i = jfileChooser.showOpenDialog(jfileChooser);
+        if (i == JFileChooser.APPROVE_OPTION){
+            String pathFile = jfileChooser.getSelectedFile().getPath();
+            persistencia.importProductos(pathFile);
+        }
+    }
+}
