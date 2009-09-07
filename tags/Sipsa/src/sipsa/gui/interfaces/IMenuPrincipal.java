c967a31f1a93c029ee9ca86b6a5277c1cecf799e
elsupergomez <elsupergomez@9ee85266-8456-11de-8b65-cf205d531c0e>
2009-09-07 02:16:55 +0000

    Fin primer iteracion

---
+++ tags/Sipsa/src/sipsa/gui/interfaces/IMenuPrincipal.java
@@ -0,0 +1,32 @@
+/*
+ * Sistemas de Informacion II 2009
+ * Proyecto Sipsa
+ */
+
+package sipsa.gui.interfaces;
+
+/**
+ * Interface a implementar para controlar el fomulario MenuPrincipal
+ * @author Claudio Rodrigo Pereyra Diaz
+ * @author Maria Eugenia Sanchez
+ */
+public interface IMenuPrincipal {
+
+    /**
+     * Accion correspondiente al boton Administrar PV
+     */
+    public void AdministrarPv();
+    /**
+     * Accion correspondiente al boton Administrar PAC
+     */
+    public void AdministrarPac();
+    /**
+     * Accion correspondiente al boton Administrar Tipos de Productos
+     */
+    public void AdministrarTipoProducto();
+    /**
+     * Accion correspondiente al boton Importar Productos
+     */
+    public void ImportarArchivoProductos();
+    
+}
