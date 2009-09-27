7579201a2899afc035d05b6ccac798713a412f32
elsupergomez <elsupergomez@9ee85266-8456-11de-8b65-cf205d531c0e>
2009-09-27 03:48:45 +0000

    Importacion Inicial Sipsa Libs

---
+++ trunk/Sipsa.Libs/src/sipsa/dominio/EstadoOT.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sipsa.dominio;
+
+/**
+ *
+ * @author elsupergomez
+ */
+public enum EstadoOT {
+    Activa,
+    Finalizada,
+    Anulada,
+}
---
+++ trunk/Sipsa.Libs/src/sipsa/presentacion/interfaces/ILogin.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sipsa.presentacion.interfaces;
+
+/**
+ *
+ * @author elsupergomez
+ */
+public interface ILogin {
+
+}
