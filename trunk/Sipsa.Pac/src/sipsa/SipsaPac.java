f738818e811777b72257fde32bc3e4947b36b14f
elsupergomez <elsupergomez@9ee85266-8456-11de-8b65-cf205d531c0e>
2009-10-23 02:22:15 +0000

    Corregido el punto en que se cerraba el socket y se finalizaba la conexion, ahora el cliente-servidor anda

--- trunk/Sipsa.Pac/src/sipsa/SipsaPac.java
+++ trunk/Sipsa.Pac/src/sipsa/SipsaPac.java
@@ -5,7 +5,13 @@
 
 package sipsa;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.UIManager;
+import sipsa.control.servicios.Conexion;
+import sipsa.control.servicios.Mensaje;
 import sipsa.presentacion.escritorio.Login;
 
 /**
@@ -21,13 +27,23 @@ public class SipsaPac {
         try {
             //Indica al UIManager que use el tema nativo del Sistema Operativo
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
+        try {
 
-        Login login = new Login();
-        login.setVisible(true);
-
+            Socket socket = new Socket("localhost", 1027);
+            Conexion conexion = new Conexion(socket);
+            Mensaje mensaje = new Mensaje();
+            mensaje.setDescriptor("hola servidor");
+            conexion.enviarMensaje(mensaje);
+            Mensaje mensajerespuesta = conexion.recibirMensaje();
+            Login login = new Login();
+            login.setVisible(true);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(SipsaPac.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(SipsaPac.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-
 }
