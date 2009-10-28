88d82e0b067c5ce11059be03c4b57122116492ef
elsupergomez <elsupergomez@9ee85266-8456-11de-8b65-cf205d531c0e>
2009-10-28 03:28:45 +0000

    Sipsa Servidor Version Candidata

--- Sipsa.Server/src/sipsa/presentacion/escritorio/ServidorGUI.java
+++ Sipsa.Server/src/sipsa/presentacion/escritorio/ServidorGUI.java
@@ -1,38 +1,37 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/*
- * ServidorGUI.java
- *
- * Created on 10/10/2009, 21:55:31
+ * Sistemas de Informacion II 2009
+ * Proyecto Sipsa
  */
 
 package sipsa.presentacion.escritorio;
 
-import java.awt.Image;
-import java.awt.Toolkit;
+import sipsa.Configuracion;
 import sipsa.control.servicios.IReceptorNotificado;
-import sipsa.control.servicios.IServicio;
+import sipsa.control.servicios.INotificador;
 
 /**
- *
- * @author elsupergomez
+ * Sesion de Cliente conectado al Servidor
+ * @author Claudio Rodrigo Pereyra Diaz
+ * @author Maria Eugenia Sanchez
  */
 public class ServidorGUI extends javax.swing.JFrame implements IReceptorNotificado {
 
-    IServicio servidor;
+    INotificador servidor;
 
-    /** Creates new form ServidorGUI */
-    public ServidorGUI(IServicio sevidor) {
-        Image img = Toolkit.getDefaultToolkit().getImage(getClass().getResource("/sipsa/presentacion/recursos/Sipsa.png"));
-        this.setIconImage(img);
-        this.setLocationRelativeTo(null);
-        this.setTitle("Servidor Sipsa");
-        this.servidor = sevidor;
+    /** Creates new form ServidorGUI
+     * @param servidor Servidor a observar notificaciones
+     */
+    public ServidorGUI(INotificador servidor) {
+        Configuracion configuracion = Configuracion.getInstancia();
+        configuracion.setEstiloLocal();
+        this.setIconImage(configuracion.getIcono());
+        this.servidor = servidor;
         servidor.addReceptorNotificado(this);
+        
         initComponents();
+
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
     }
 
     /** This method is called from within the constructor to
@@ -44,33 +43,26 @@ public class ServidorGUI extends javax.swing.JFrame implements IReceptorNotifica
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton4 = new javax.swing.JButton();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jTextArea1 = new javax.swing.JTextArea();
-        jButton1 = new javax.swing.JButton();
+        jButtonSalir = new javax.swing.JButton();
+        jScrollPaneEventos = new javax.swing.JScrollPane();
+        jTextAreaEventos = new javax.swing.JTextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Servidor Sipsa");
+        setName("SipsaServerGui"); // NOI18N
 
-        jButton4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/org/tango-project/tango-icon-theme/16x16/actions/system-log-out.png"))); // NOI18N
-        jButton4.setText("Salir");
-        jButton4.addActionListener(new java.awt.event.ActionListener() {
+        jButtonSalir.setIcon(new javax.swing.ImageIcon(getClass().getResource("/org/tango-project/tango-icon-theme/16x16/actions/system-log-out.png"))); // NOI18N
+        jButtonSalir.setText("Salir");
+        jButtonSalir.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton4ActionPerformed(evt);
+                jButtonSalirActionPerformed(evt);
             }
         });
 
-        jTextArea1.setColumns(20);
-        jTextArea1.setEditable(false);
-        jTextArea1.setRows(5);
-        jScrollPane1.setViewportView(jTextArea1);
-
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/org/tango-project/tango-icon-theme/16x16/actions/media-playback-start.png"))); // NOI18N
-        jButton1.setText("Iniciar");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
+        jTextAreaEventos.setColumns(20);
+        jTextAreaEventos.setEditable(false);
+        jTextAreaEventos.setRows(5);
+        jScrollPaneEventos.setViewportView(jTextAreaEventos);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -79,48 +71,40 @@ public class ServidorGUI extends javax.swing.JFrame implements IReceptorNotifica
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, 151, Short.MAX_VALUE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 155, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 312, Short.MAX_VALUE))
+                    .addComponent(jButtonSalir, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 444, Short.MAX_VALUE)
+                    .addComponent(jScrollPaneEventos, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 444, Short.MAX_VALUE))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 250, Short.MAX_VALUE)
+                .addComponent(jScrollPaneEventos, javax.swing.GroupLayout.DEFAULT_SIZE, 250, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jButton4)
-                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addComponent(jButtonSalir)
                 .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+    private void jButtonSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonSalirActionPerformed
         this.servidor.removeReceptorNotificado(this);
         this.setVisible(false);
         System.exit(0);
-    }//GEN-LAST:event_jButton4ActionPerformed
-
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        jButton1.setEnabled(false);
-        servidor.iniciar();
-    }//GEN-LAST:event_jButton1ActionPerformed
+}//GEN-LAST:event_jButtonSalirActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton4;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JButton jButtonSalir;
+    private javax.swing.JScrollPane jScrollPaneEventos;
+    private javax.swing.JTextArea jTextAreaEventos;
     // End of variables declaration//GEN-END:variables
 
+    /**
+     * Realiza las tareas correspondientes al recibir una natificacion
+     * @param notificacion
+     */
     public void procesarNotificacion(String notificacion) {
-        this.jTextArea1.append(notificacion + "\n");
+        this.jTextAreaEventos.append(notificacion + "\n");
     }
-
 }
