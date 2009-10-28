c781cb2bfb2d6cf0dbdbbd5c40a1154a997d1646
elsupergomez <elsupergomez@9ee85266-8456-11de-8b65-cf205d531c0e>
2009-10-28 03:29:42 +0000

    Sipsa Servidor Version Candidata

--- Sipsa.Server/src/sipsa/SipsaServidor.java
+++ Sipsa.Server/src/sipsa/SipsaServidor.java
@@ -1,55 +1,41 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Sistemas de Informacion II 2009
+ * Proyecto Sipsa
  */
 
 package sipsa;
 
-import javax.swing.UIManager;
 import sipsa.control.servicios.Servidor;
 import sipsa.presentacion.escritorio.ServidorGUI;
 
 /**
- *
- * @author elsupergomez
+ * Aplicacion Servidor de Sipsa
+ * @author Claudio Rodrigo Pereyra Diaz
+ * @author Maria Eugenia Sanchez
  */
 public class SipsaServidor {
-    private static boolean demonio = false;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        try {
-            //Indica al UIManager que use el tema nativo del Sistema Operativo
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-
-        // TODO manejar los argumentos, y dar posibilidad a daemonizar o usar frontend
         Servidor servidor = new Servidor();
         for (int i = 0; i < args.length; i++) {
             String string = args[i];
-            if (string.equalsIgnoreCase("-d")){
-                demonio = true;
+            if (string.equalsIgnoreCase("-g")){
+                new ServidorGUI(servidor);
             } else if (string.startsWith("-p=")){
                 int puerto = Integer.valueOf(string.substring(3));
                 servidor.setPuerto(puerto);
             } else {
                 StringBuilder ayuda = new StringBuilder();
-                ayuda.append("-d       --> Servidor en modo Demonio");
-                ayuda.append("-p=<nro> --> Indica en <nro> el puerto escucha del servidor");
-                ayuda.append("-h       --> Muestra este mensaje de ayuda");
+                ayuda.append("Ayuda de parametros del Servidor Sipsa:\n");
+                ayuda.append("  -g       --> Inicia el servidor con interface grafica\n");
+                ayuda.append("  -p=<nro> --> Indica en <nro> el puerto escucha del servidor, por defecto p=1027\n");
+                ayuda.append("  -h       --> Muestra este mensaje de ayuda\n");
                 System.out.println(ayuda.toString());
                 System.exit(1);
             }
         }
-        if (SipsaServidor.demonio){
-            servidor.setDaemon(demonio);
-            servidor.iniciar();
-        } else {
-            ServidorGUI servidorGUI = new ServidorGUI(servidor);
-            servidorGUI.setVisible(true);
-        }
+        servidor.iniciar();
     }
 }
