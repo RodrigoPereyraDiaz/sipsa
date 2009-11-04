478fbdcf88425f8674170db23ad2fc632b3a2fef
elsupergomez <elsupergomez@9ee85266-8456-11de-8b65-cf205d531c0e>
2009-11-04 01:50:39 +0000

    Creacion del punto de iteracion 2

--- Sipsa.Libs/src/sipsa/control/servicios/INotificador.java
+++
@@ -1,9 +0,0 @@
-package sipsa.control.servicios;
-
-import sipsa.control.servicios.*;
-
-public interface INotificador {
-    public void notificar(String mensaje);
-    public void addReceptorNotificado(IReceptorNotificado iReceptorNotificado);
-    public void removeReceptorNotificado(IReceptorNotificado iReceptorNotificado);
-}
