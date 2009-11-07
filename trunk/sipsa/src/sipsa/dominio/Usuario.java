4d7d567fd3d387c415fddea05b9b98c31950b406
elsupergomez <elsupergomez@9ee85266-8456-11de-8b65-cf205d531c0e>
2009-11-07 20:04:41 +0000

    Se Agrega el proyecto de Sipsa Web para puntos de Venta

--- trunk/sipsa/src/sipsa/dominio/Usuario.java
+++ trunk/sipsa/src/sipsa/dominio/Usuario.java
@@ -11,9 +11,43 @@ package sipsa.dominio;
  */
 public class Usuario {
 
+    private String usuario;
+    private char[] password;
+
     public boolean isValido(){
-        //TODO
-        return true;
+        if (this.usuario.equals("sipsa")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * @return the usuario
+     */
+    public String getUsuario() {
+        return usuario;
+    }
+
+    /**
+     * @param usuario the usuario to set
+     */
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    /**
+     * @return the password
+     */
+    public char[] getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(char[] password) {
+        this.password = password;
     }
 
 }
